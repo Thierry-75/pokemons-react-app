21097f98f925921d1b53e0a615ad6ec0c810d4b4
thierry dothee <thierrydothee@protonmail.com>
2023-10-29 20:58:20 +0100

    postcard with materialize 12 pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemon';
 
+
+
 /*
 règles : 
  ->  appeler les hooks uniquement au niveau racine, 
  ->  appeler les hooks uniquement depuis des composants de fonctions,
- ->  modifier un état par remplacement 
+ ->   modifier un état par remplacement 
 */
 const App: FunctionComponent = () => {
  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -15,13 +17,38 @@ const App: FunctionComponent = () => {
 
  },[]);
 
+ // cette constante est en fait notre gestionnaire d'évènement. exemple sur onClick
+ //const showPokemonsCount = () => {
+ //   console.log("il y a " + pokemons.length + " pokemons dans le Pokédex" );
+
+ //};
     
  return (
+  
   <div>
-    <h1>Pokédex</h1>
-    <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+    <h1 className="center">Pokédex</h1>
+    <div className="container">
+      <div className="row">
+        {pokemons.map(({ id, name, picture, created })=>(
+          <div className="col s6 m4" key={id}>
+            <div className="card horizontal">
+              <div className="card-image">
+                <img src={picture} alt={name} />
+              </div>
+              <div className="card-stacked">
+                <div className="card-content">
+                  <p>{name}</p>
+                  <p><small>{created.toString()}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   </div>
- )
+
+ );
 }
   
 export default App;
