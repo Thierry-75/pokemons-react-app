247866d2615753f34bdb268efadf11801a3c40dc
thierry dothee <thierrydothee@protonmail.com>
2023-10-30 10:14:47 +0100

    add component card add page cardlist with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent, useState, useEffect } from 'react';
-import Pokemon from './models/pokemon';
-import POKEMONS from './models/mock-pokemon';
+import React, { FunctionComponent } from 'react';
+import Pokemonlist from './pages/pokemon-list';
+
 
 
 
@@ -11,11 +11,7 @@ règles :
  ->   modifier un état par remplacement 
 */
 const App: FunctionComponent = () => {
- const [pokemons, setPokemons] = useState<Pokemon[]>([]);
- useEffect(()=> {
-    setPokemons(POKEMONS);
 
- },[]);
 
  // cette constante est en fait notre gestionnaire d'évènement. exemple sur onClick
  //const showPokemonsCount = () => {
@@ -24,31 +20,8 @@ const App: FunctionComponent = () => {
  //};
     
  return (
-  
-  <div>
-    <h1 className="center">Pokédex</h1>
-    <div className="container">
-      <div className="row">
-        {pokemons.map(({ id, name, picture, created })=>(
-          <div className="col s6 m4" key={id}>
-            <div className="card horizontal">
-              <div className="card-image">
-                <img src={picture} alt={name} />
-              </div>
-              <div className="card-stacked">
-                <div className="card-content">
-                  <p>{name}</p>
-                  <p><small>{created.toString()}</small></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  </div>
-
- );
+  <Pokemonlist />
+ )
 }
   
 export default App;
